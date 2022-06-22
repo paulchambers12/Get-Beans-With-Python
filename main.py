192fa51da204a4e95d066e63155a30a502776aa1
paulchambers12 <paulchambers12@gmail.com>
2022-06-22 21:15:57 +0100

    Create main.py
    Base frame working for the main.py. All current code is placeholders

---
+++ main.py
@@ -0,0 +1,17 @@
+#credits and shit go here
+
+
+#includes go here
+from collections import Counter
+
+
+#methods get initiallized here
+def swap_case(s):
+    thing = ("".join(c.upper() if c.islower() else c.lower() for c in s))
+    return thing
+
+#main method
+if __name__ == '__main__':
+    s = raw_input()
+    result = swap_case(s)
+    print result
