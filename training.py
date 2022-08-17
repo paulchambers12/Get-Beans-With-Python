315b495b63deeca90acfa83cde5f6d6d8ebf6ea4
paulchambers12 <paulchambers12@gmail.com>
2022-08-17 21:00:59 +0100

    added training_2

--- training.py
+++ training.py
@@ -1,10 +1,20 @@
 import requests
 from bs4 import BeautifulSoup
-page = requests.get("https://dataquestio.github.io/web-scraping-pages/simple.html")
+#page = requests.get("https://dataquestio.github.io/web-scraping-pages/simple.html")
+page = requests.get("https://dataquestio.github.io/web-scraping-pages/ids_and_classes.html")
 #print("page: {0}".format(page))
 #print("page.status_code: {0}".format(page.status_code))
 #print("page.content: {0}".format(page.content))
 
 soup = BeautifulSoup(page.content, 'html.parser')
-print(soup.prettify())
-print("list: {0}".format(list(soup.children)))
+#pretty_soup = soup.prettify()
+#print(soup.prettify())
+#print(soup.find_all('p', class_='outer-text'))
+#print(soup.find_all(class_="outer-text"))
+print(soup.find_all(id="first"))
+#print("********list: {0}".format(list(soup.children)))
+#print(list(soup.children))
+#print([type(item) for item in list(soup.children)])
+#print(page.status_code)
+#print(page.content)
+#print(page.prettify())
---
+++ training_2.py
@@ -0,0 +1,9 @@
+import requests
+from bs4 import BeautifulSoup
+
+page = requests.get("https://forecast.weather.gov/MapClick.php?lat=37.7772&lon=-122.4168")
+soup = BeautifulSoup(page.content, 'html.parser')
+seven_day = soup.find(id="seven-day-forecast")
+forecast_items = seven_day.find_all(class_="tombstone-container")
+tonight = forecast_items[0]
+print(tonight.prettify())
